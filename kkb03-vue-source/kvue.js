3b8d8840a0b05ac49a57ab0ab733e97b8165218d
xjl <353167732@qq.com>
2020-01-08 00:05:40 +0800

    vue原理基本实现

---
+++ kkb03-vue-source/kvue.js
@@ -0,0 +1,40 @@
+// new Vue({data:{...}})
+
+class Vue {
+    constructor(options) {
+        this.$options = options;
+
+
+        // 数据响应化
+        this.$data = options.data;
+        this.observe(this.$data);
+    }
+
+    observe(value) {
+        if(!value || typeof value !== 'object') {
+            return;
+        }
+
+        // 遍历对象
+        Object.keys(value).forEach(key=>{
+            this.defineReactive(value, key, value[key]);
+        });
+    }
+
+    // 数据响应化
+    defineReactive(value, key, val) {
+        Object.defineProperty(obj, key, {
+            get() {
+                return val;
+            },
+            set(newVal) {
+                if(newVal === val) {
+                    return;
+                }
+                val = newVal;
+                console.log(`${key}属性更新为${val}`);
+            }
+        })
+    }
+
+}
